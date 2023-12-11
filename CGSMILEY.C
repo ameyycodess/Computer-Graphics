7f360de827b30b29ef1f786e12ea9c1f52c33ee8
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2023-12-11 22:19:50 +0530

    Add files via upload

--- CGSMILEY.C
+++ CGSMILEY.C
@@ -4,19 +4,30 @@ void main()
 {
  int gd=DETECT,gm,xcen,ycen;
  initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+
  xcen=getmaxx()/2;
  ycen=getmaxy()/2;
  //main
  circle(xcen,ycen,150);
- //smile
- ellipse(xcen,ycen+50,180,360,80,50);
+ setfillstyle(1,YELLOW);
+ floodfill(xcen,ycen,15);
+
  //eyes
- circle(xcen-50,ycen-50,30);
- circle(xcen+50,ycen-50,30);
+ ellipse(xcen-50,ycen-50,0,360,18,23);
+ setfillstyle(1,BLACK);
+ floodfill(xcen-50,ycen-50,15);
 
- circle(xcen-50,ycen-50,10);
- circle(xcen+50,ycen-50,10);
+ ellipse(xcen+50,ycen-50,0,360,18,23);
+ setfillstyle(1,BLACK);
+ floodfill(xcen+50,ycen-50,15);
 
+
+
+
+ //smile
+ setcolor(BLACK);
+ ellipse(xcen,ycen+50,180,360,80,50);
+ ellipse(xcen,ycen+50,180,360,80,25);
 
  getch();
  closegraph();
